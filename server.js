6c1b00e24999297bd5098d06a05ad7f67a56d3a7
Luis Fernandez <lfernandez@qvision.com.co>
2021-09-28 23:23:07 -0500

    Config deploy heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.static(__dirname+'/dist/front'))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/dist/front/index.html'));
+});
+app.listen(PORT || 8080)
